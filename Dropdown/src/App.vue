900d1fa70f1f62ec0b1c8258350255cf13d141c3
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2023-01-13 10:11:03 -0300

    add new pratice 'Dropdown'

---
+++ Dropdown/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <header>
+      <Navbar />
+    </header>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+header {
+  width: 100vw;
+  background-color: #303030;
+  padding: 15px;
+}
+
+</style>
